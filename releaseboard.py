f63d98e18cfb9c1165e2be55fa81c1a8e6991a96
Brendan Bagley <bbagley@gannett.com>
2015-10-26 11:47:45 -0400

    [NEW] skeleton of main file

---
+++ releaseboard.py
@@ -0,0 +1,18 @@
+"""Script to get build information from Jenkins"""
+import sys
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+
+def get_recent_release_job_information(url):
+    """Return a list PROD release jobs with associated versions."""
+    pass
+
+def get_latest_build_information(url):
+    """Return a list build jobs with recent versions and success or failure."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
